0b321812b104d435d3df024f4034320e3cf82df5
dhaifullahtohari <dhaifto@gmail.com>
2025-07-25 03:36:59 +0300

    Add admin route to urls.py2

--- HoneyStore/urls.py
+++ HoneyStore/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     # لوحة التحكم
@@ -17,4 +19,4 @@ urlpatterns = [
 
 # دعم عرض ملفات media أثناء التطوير
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
